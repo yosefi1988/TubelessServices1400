f3068ef4a3232748465fb7b58c414d17d5c2aaf5
adminSajjad <yosefi1988@gmail.com>
2025-02-22 12:24:41 +0330

    landing Done

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -17,11 +17,15 @@ namespace TubelessServices
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      //"~/Scripts/bootstrap.js"
+                      "~/Content/bootstrap-rtl/js/bootstrap.min.js"
+                      //"~/Scripts/respond.js"
+                      ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      //"~/Content/bootstrap.css",
+                      //"~/Content/bootstrap.rtl.css",
+                      "~/Content/bootstrap-rtl/css/bootstrap.rtl.css",
                       "~/Content/site.css"));
         }
     }
